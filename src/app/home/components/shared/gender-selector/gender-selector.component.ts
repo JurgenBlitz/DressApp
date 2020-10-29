46f9cae9057cb5c92cab4f7e146f552772ec4f96
Jorge <jorge.gopae@gmail.com>
2020-10-29 19:33:05 +0100

    Included base README, ng-translate and base translation files

--- src/app/home/components/shared/gender-selector/gender-selector.component.ts
+++ src/app/home/components/shared/gender-selector/gender-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-gender-selector',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gender-selector.component.scss'],
 })
 export class GenderSelectorComponent implements OnInit {
+
+  @Output() genderSelection = new EventEmitter<any>();
 
   constructor() { }
 
